640f0bd135626c89526e99bed188fb1b5ec45f8f
xixi zhou <admin@zhouchenxi.cn>
2018-12-05 14:17:04 +0800

    增加跳转逻辑

--- index.php
+++ index.php
@@ -6,19 +6,19 @@
  * Time: 10:14
  */
 foreach ($_GET as $key => $item) {
-    if($key != 'redirect_uri'){
-        $params .= '&'.$key . '=' . $item;
+    if ($key != 'to') {
+        $params .= '&' . $key . '=' . $item;
     }
 }
-if (empty($_GET['redirect_uri'])) {
+if (empty($_GET['to'])) {
     header('Location: https://www.17duu.com');
-}else{
-    if(empty($params)){
-        $url = $_GET['redirect_uri'];
-    }else{
-        $redirectUri = urldecode($_GET['redirect_uri']);
-        $redirectUriArray = explode('?',$redirectUri);
-        $url = $redirectUriArray[0].'?'.$params.'&'.$redirectUriArray[1];
+} else {
+    if (empty($params)) {
+        $url = $_GET['to'];
+    } else {
+        $redirectUri = urldecode($_GET['to']);
+        $redirectUriArray = explode('?', $redirectUri);
+        $url = $redirectUriArray[0] . '?' . $params . '&' . $redirectUriArray[1];
     }
-    header('Location:'.$url);
+    header('Location:' . $url);
 }
