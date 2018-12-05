52dd2194ff72d770fb34c91da1799bc3c9ec3749
xixi zhou <admin@zhouchenxi.cn>
2018-12-05 10:24:03 +0800

    init

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: xixi
+ * Date: 2018/12/5
+ * Time: 10:14
+ */
+//if(empty($_GET['redirect_uri'])){
+    header('Location: https://www.17duu.com'.$request_uri);
+//}
