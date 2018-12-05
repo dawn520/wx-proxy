12686c39548b0acd5fb05c485142645c0b6d1b39
xixi zhou <admin@zhouchenxi.cn>
2018-12-05 11:12:59 +0800

    增加跳转逻辑

--- index.php
+++ index.php
@@ -5,6 +5,16 @@
  * Date: 2018/12/5
  * Time: 10:14
  */
-//if(empty($_GET['redirect_uri'])){
-    header('Location: https://www.17duu.com'.$request_uri);
-//}
+foreach ($_GET as $key => $item) {
+    $params .= '&'.$key . '=' . $item;
+}
+if (empty($_GET['redirect_uri'])) {
+    header('Location: https://www.17duu.com');
+}else{
+    if(empty($params)){
+        $url = $_GET['redirect_uri'];
+    }else{
+        $url = $_GET['redirect_uri'].'?'.$params;
+    }
+    header('Location:'.$url);
+}
